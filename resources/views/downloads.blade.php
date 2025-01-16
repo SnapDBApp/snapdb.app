bc80a8176141feeeb2ca2e6de55f849bec38a43c
musa11971 <mussesemou99@gmail.com>
2025-01-16 16:11:26 +0100

    add download btn

--- resources/views/downloads.blade.php
+++ resources/views/downloads.blade.php
@@ -8,7 +8,10 @@
         <x-slot:body>
             <div id="releases" class="flex flex-col gap-4">
                 @foreach ($releases as $i => $release)
-                    @php($isLatest = $i === 0)
+                    @php
+                        $isLatest = $i === 0;
+                        $downloadURL = 'https://github.com/SnapDBApp/app/releases/download/' . $release['tag_name'] . '/SnapDB.zip';
+                    @endphp
                     <x-card
                         id="release-{{ $release['tag_name'] }}"
                         @class([
@@ -17,7 +20,7 @@
                         ])
                     >
                         <div class="flex gap-2">
-                            <h1 class="text-xl flex-1">
+                            <h1 class="text-xl flex-1 flex items-center">
                                 {{ $release['name'] }}
 
                                 @if ($isLatest)
@@ -26,10 +29,26 @@
                                     <x-tag.red>Outdated</x-tag.red>
                                 @endif
                             </h1>
-                            <span>{{ Carbon\Carbon::parse($release['published_at'])->format('Y-m-d H:i') }}</span>
+                            <div>
+                                @if($isLatest)
+                                    <x-btn.primary x-data
+                                                   @click="window.location = '{{ $downloadURL }}'"
+                                    >
+                                        Download
+                                    </x-btn.primary>
+                                @endif
+                            </div>
                         </div>
 
-                        <x-markdown class="markdown-body my-4">{{ $release['body'] }}</x-markdown>
+                        <x-markdown class="markdown-body my-4">
+                            @if(empty($release['body']))
+                                <span class="text-gray-500">No release notes provided.</span>
+                            @else
+                                {{ $release['body'] }}
+                            @endif
+                        </x-markdown>
+
+                        <span class="text-gray-400">{{ Carbon\Carbon::parse($release['published_at'])->format('Y-m-d H:i') }}</span>
                     </x-card>
                 @endforeach
             </div>
