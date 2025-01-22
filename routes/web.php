99e47e254a629dcc32ec8b25b44da9b192083569
musa11971 <mussesemou99@gmail.com>
2025-01-22 09:22:59 +0100

    add cache routes

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,25 @@ use App\Http\Controllers\ManageLicenseController;
 use App\Http\Controllers\SupportedDatabaseController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'landing')->name('landing');
-Route::view('terms', 'legal.terms-conditions')->name('terms-conditions');
-Route::view('returns', 'legal.returns-policy')->name('returns-policy');
-Route::view('privacy', 'legal.privacy-policy')->name('privacy-policy');
-Route::get('downloads', DownloadController::class)->name('downloads');
-Route::get('contact', [ContactController::class, 'index'])->name('contact');
-Route::post('contact', [ContactController::class, 'sendMessage']);
+// Cached routes
+Route::middleware('cache.headers:public;max_age=3600;etag')->group(function () {
+    Route::view('/', 'landing')->name('landing');
+    Route::view('terms', 'legal.terms-conditions')->name('terms-conditions');
+    Route::view('returns', 'legal.returns-policy')->name('returns-policy');
+    Route::view('privacy', 'legal.privacy-policy')->name('privacy-policy');
+    Route::get('downloads', DownloadController::class)->name('downloads');
+    Route::get('contact', [ContactController::class, 'index'])->name('contact');
+    Route::post('contact', [ContactController::class, 'sendMessage']);
 
-Route::group([
-    'prefix' => 'databases',
-], function () {
-    Route::get('/', [SupportedDatabaseController::class, 'index'])
-        ->name('supported-databases');
+    Route::group([
+        'prefix' => 'databases',
+    ], function () {
+        Route::get('/', [SupportedDatabaseController::class, 'index'])
+            ->name('supported-databases');
 
-    Route::get('{slug}', [SupportedDatabaseController::class, 'show'])
-        ->name('supported-databases.show');
+        Route::get('{slug}', [SupportedDatabaseController::class, 'show'])
+            ->name('supported-databases.show');
+    });
 });
 
 Route::group([
