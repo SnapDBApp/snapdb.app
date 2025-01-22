b38a15f15e697ed9bc7760d17235e802a4673ca9
musa11971 <mussesemou99@gmail.com>
2025-01-22 09:37:18 +0100

    static landing page

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,29 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DownloadController;
 use App\Http\Controllers\ManageLicenseController;
 use App\Http\Controllers\SupportedDatabaseController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-// Cached routes
-Route::middleware('cache.headers:public;max_age=86400;etag')->group(function () {
-    Route::view('/', 'landing')->name('landing');
-    Route::view('terms', 'legal.terms-conditions')->name('terms-conditions');
-    Route::view('returns', 'legal.returns-policy')->name('returns-policy');
-    Route::view('privacy', 'legal.privacy-policy')->name('privacy-policy');
-    Route::get('downloads', DownloadController::class)->name('downloads');
-    Route::get('contact', [ContactController::class, 'index'])->name('contact');
-    Route::post('contact', [ContactController::class, 'sendMessage']);
+// We disable CSRF protection for the landing page because it's a static page.
+// This allows the landing page to be cached by Cloudflare.
+Route::view('/', 'landing')->name('landing')
+    ->withoutMiddleware([VerifyCsrfToken::class]);
 
-    Route::group([
-        'prefix' => 'databases',
-    ], function () {
-        Route::get('/', [SupportedDatabaseController::class, 'index'])
-            ->name('supported-databases');
+Route::view('terms', 'legal.terms-conditions')->name('terms-conditions');
+Route::view('returns', 'legal.returns-policy')->name('returns-policy');
+Route::view('privacy', 'legal.privacy-policy')->name('privacy-policy');
+Route::get('downloads', DownloadController::class)->name('downloads');
+Route::get('contact', [ContactController::class, 'index'])->name('contact');
+Route::post('contact', [ContactController::class, 'sendMessage']);
 
-        Route::get('{slug}', [SupportedDatabaseController::class, 'show'])
-            ->name('supported-databases.show');
-    });
+Route::group([
+    'prefix' => 'databases',
+], function () {
+    Route::get('/', [SupportedDatabaseController::class, 'index'])
+        ->name('supported-databases');
+
+    Route::get('{slug}', [SupportedDatabaseController::class, 'show'])
+        ->name('supported-databases.show');
 });
 
 Route::group([
